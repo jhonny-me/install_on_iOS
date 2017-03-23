9f584e5d4641bc6a526a5dab4dfbd476bd38e880
Johnny Gu <johnny@wiredcraft.com>
2017-03-24 00:23:37 +0800

    can edit device alias now

--- Access/Access/DeviceManager.swift
+++ Access/Access/DeviceManager.swift
@@ -34,7 +34,7 @@ class DeviceManager {
 }
 extension DeviceManager {
     
-    func searchForIOSDevices() -> [String] {
+    func searchForIOSDevices() -> [Phone] {
         let path = "/usr/local/bin/mobiledevice"
         let process = Process()
         let output = Pipe()
@@ -47,9 +47,52 @@ extension DeviceManager {
         guard let string = String.init(data: data, encoding: .utf8) else { return [] }
         let array = string.components(separatedBy: "\n").dropLast()
         let uuids = [String](array)
-        print("devices: \(array)")
-        AppDelegate.devices = uuids.map(Phone.init)
-        return uuids
+        let jsons = uuids.map { uuid -> [String : String] in
+            let extras = ["ProductVersion", "ProductType", "DeviceName"].map({ key in
+                return getExtraInfo(key: key, for: uuid)
+            })
+            // check model
+            var model: String
+            switch extras[1] {
+                case "iPhone4,1":
+                    model = "iPhone 4S"
+                case "iPhone5,1", "iPhone5,2":
+                    model = "iPhone 5"
+                case "iPhone5,3", "iPhone5,4":
+                    model = "iPhone 5c"
+                case "iPhone6,1", "iPhone6,2":
+                    model = "iPhone 5s"
+                case "iPhone7,1":
+                    model = "iPhone 6 Plus"
+                case "iPhone7,2":
+                    model = "iPhone 6"
+                case "iPhone8,1":
+                    model = "iPhone 6s"
+                case "iPhone8,2":
+                    model = "iPhone 6s plus"
+            default:
+                model = "unknown"
+            }
+            
+            return ["uuid": uuid, "alias": extras[2], "model": model, "system": extras[0]]
+        }
+        let devices = jsons.flatMap(Phone.init)
+        AppDelegate.devices = devices
+        return devices
+    }
+    
+    func getExtraInfo(key: String, for id: String) -> String {
+        let path = "/usr/local/bin/mobiledevice"
+        let process = Process()
+        let output = Pipe()
+        process.standardOutput = output
+        process.launchPath = path
+        process.arguments = ["get_device_prop", "-u", id, key]
+        process.launch()
+        process.waitUntilExit()
+        let data = output.fileHandleForReading.readDataToEndOfFile()
+        guard let string = String.init(data: data, encoding: .utf8) else { return "" }
+        return string.trimmingCharacters(in: ["\n"])
     }
     
     func searchForAndroidDevices() -> [String] {
@@ -144,9 +187,12 @@ enum Log {
 }
 
 struct Phone {
-    var uuid: String
+    let uuid: String
     var alias: String = ""
     var type: DeviceManager.DeviceType = .iOS
+    var model: String = ""
+    var system: String = ""
+    var appInstalled: Bool = false
 }
 
 extension Phone {
@@ -159,10 +205,13 @@ extension Phone {
         self.uuid = uuid
         alias = json["alias"] ?? ""
         type = DeviceManager.DeviceType(rawValue: json["type"] ?? "iOS")!
+        model = json["model"] ?? ""
+        system = json["system"] ?? ""
+        appInstalled = Bool(json["appInstalled"] ?? "false") ?? false
     }
     
     func archive() -> [String: String] {
-        return ["uuid": uuid, "alias": alias, "type": type.rawValue]
+        return ["uuid": uuid, "alias": alias, "type": type.rawValue, "model": model, "system": system, "appInstalled": appInstalled.description]
     }
 }
 
--- Access/Access/DevicesViewController.swift
+++ Access/Access/DevicesViewController.swift
@@ -12,25 +12,32 @@ class DevicesViewController: NSViewController {
 
     @IBOutlet weak var button: NSButton!
     @IBOutlet weak var tableView: NSTableView!
-    var uuids: [String] = []
+    var uuids: [Phone] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        uuids = AppDelegate.devices
+        tableView.reloadData()
+    }
+    
+    override func viewDidDisappear() {
+        super.viewDidDisappear()
+        AppDelegate.devices = uuids
+    }
+    
     @IBAction func searchDevices(_ sender: Any) {
 
-        guard let uuids = (try? DeviceManager(type: .iOS).start(.search)) as? [String] else { return }
+        guard let uuids = (try? DeviceManager(type: .iOS).start(.search)) as? [Phone] else { return }
         self.uuids = uuids
         tableView.reloadData()
     }
 
-    @IBAction func installAction(_ sender: Any) {
-//        let appPath = AppDelegate.downloadPath + "/Starbucks.ipa"
-//        uuids.forEach { (uuid) in
-//            print(DeviceManager.install(with: appPath, on: uuid))
-//            
-//        }
+    @IBAction func historyAction(_ sender: Any) {
+        
     }
 }
 
@@ -40,8 +47,46 @@ extension DevicesViewController: NSTableViewDataSource, NSTableViewDelegate {
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        guard let cell = tableView.make(withIdentifier: "BaseCell", owner: self) as? NSTableCellView else { return NSView() }
-        cell.textField?.stringValue = uuids[row]
-        return cell
+        
+        if tableColumn == tableView.tableColumns[0] {
+            guard let cell = tableView.make(withIdentifier: "TextCell", owner: self) as? NSTableCellView else { return NSView() }
+            cell.textField?.stringValue = uuids[row].uuid
+            return cell
+        }else {
+            guard let cell = tableView.make(withIdentifier: "EditableTextFieldCellView", owner: self) as? EditableTextFieldCellView else { return NSView() }
+            if tableColumn == tableView.tableColumns[1] {
+                cell.textField?.stringValue = uuids[row].alias
+                cell.textDidChangeCallback = { [unowned self] string in
+                    self.uuids[row].alias = string
+                }
+            }else if tableColumn == tableView.tableColumns[2] {
+                cell.textField?.stringValue = uuids[row].type.rawValue
+                cell.textDidChangeCallback = { [unowned self] string in
+                    self.uuids[row].type = DeviceManager.DeviceType(rawValue: string) ?? .iOS
+                }
+            }else if tableColumn == tableView.tableColumns[3] {
+                cell.textField?.stringValue = uuids[row].model
+                cell.textDidChangeCallback = { [unowned self] string in
+                    self.uuids[row].model = string
+                }
+            }else if tableColumn == tableView.tableColumns[4] {
+                cell.textField?.stringValue = uuids[row].system
+                cell.textDidChangeCallback = { [unowned self] string in
+                    self.uuids[row].system = string
+                }
+            }
+            return cell
+        }
+    }
+}
+
+class EditableTextFieldCellView: NSTableCellView {
+    var textDidChangeCallback: ((String) -> Void)?
+}
+
+extension EditableTextFieldCellView: NSTextFieldDelegate {
+    override func controlTextDidChange(_ obj: Notification) {
+        guard let textfield = obj.object as? NSTextField else { return }
+        textDidChangeCallback?(textfield.stringValue)
     }
 }
