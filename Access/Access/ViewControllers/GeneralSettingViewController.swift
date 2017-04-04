8b16da4d9474c184672d3d791cc652b90b06887d
Johnny Gu <johnny@wiredcraft.com>
2017-04-04 16:48:31 +0800

    add config working dir function

---
+++ Access/Access/ViewControllers/GeneralSettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GeneralSettingViewController.swift
+//  Access
+//
+//  Created by Johnny Gu on 04/04/2017.
+//  Copyright © 2017 Johnny Gu. All rights reserved.
+//
+
+import Cocoa
+
+class GeneralSettingViewController: NSViewController {
+    
+    @IBOutlet weak var workDirTextField: NSTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        workDirTextField.stringValue = AppDelegate.downloadPath
+    }
+    
+    @IBAction func showInFinder(_ sender: Any) {
+        let path = URL(string: AppDelegate.downloadPath + "/")!
+        NSWorkspace.shared().activateFileViewerSelecting([path])
+    }
+}
+
+extension GeneralSettingViewController: NSTextFieldDelegate {
+    override func controlTextDidChange(_ obj: Notification) {
+        guard let textfield = obj.object as? NSTextField else { return }
+        if textfield == workDirTextField {
+            AppDelegate.downloadPath = workDirTextField.stringValue
+        }
+    }
+}
